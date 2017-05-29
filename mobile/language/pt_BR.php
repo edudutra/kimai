f17ba711440d20e5ad7680d583af83666a3592c7
Eduardo Dutra <eduardo.dutra@habibs.com.br>
2017-05-29 09:07:44 -0300

    First Commit

---
+++ mobile/language/pt_BR.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'login_title'       => 'Please login',
+    'login_username'    => 'Usuário',
+    'login_password'    => 'Senha',
+    'login_btn'         => 'Login',
+    'task_title'        => 'Selecione a tarefa que deseja iniciar',
+    'choose_project'    => 'Selecinone projeto:',
+    'choose_task'       => 'Selecione tarefa:',
+    'stop'              => 'Parar',
+    'start'             => 'Iniciar',
+    'error'             => 'Erro',
+    'error_tasks'       => 'A lista de tarefas não pode ser carregada',
+    'error_projects'    => 'A lista de projetos não pode ser carregada',
+    'working_time'      => 'Você está trabalhando a: {0}'
+);
